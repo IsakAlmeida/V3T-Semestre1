35b3dccc3fe4145700194fc4270211993a5d773b
Lucas-Frossi <lucas.teodoro@sptech.school>
2025-10-07 22:56:24 -0300

    Inicializando projeto: adicionando HTML, JS e estrutura inicial de CSS

---
+++ scripts/script-dashboard.js
@@ -0,0 +1,62 @@
+const ctxTemperatura = document.getElementById('chartTemperatura');
+
+new Chart(ctxTemperatura, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: 'Temperatura °C',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+const ctxUmidade = document.getElementById('chartUmidade');
+
+new Chart(ctxUmidade, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: 'Umidade',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+const ctxAlertas = document.getElementById('chartAlertas');
+
+new Chart(ctxAlertas, {
+    type: 'bar',
+    data: {
+        labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+        datasets: [{
+            label: 'Umidade',
+            data: [12, 19, 3, 5, 2, 3, 8, 8, 6, 5, 4, 7],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
