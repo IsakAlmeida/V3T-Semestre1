a74638ccc284c85624f98387110e4e33a59ca38b
RayzaDSbr <127635010+RayzaDSbr@users.noreply.github.com>
2025-10-20 16:48:29 -0300

    alterações na tela de login e cadastro
    alteração nas validações e na imagem de olhinho conforme alinhado com grupo e com fernanda

--- Empresa/Equipe Atual/Site/scripts/script-login.js
+++ Empresa/Equipe Atual/Site/scripts/script-login.js
@@ -16,7 +16,7 @@ function mostrarSenha(idInput, icone) {
         input.type = "text";
         // altera o atributo 'type' do input para "text", ou seja, torna a senha visível como texto normal no campo.
 
-        icone.src = "../imgs/olho-aberto-senha.png";
+        icone.src = "../imgs/olho-fechado-senha.png";
         // altera a propriedade 'src' do elemento 'icone' para a imagem do olho aberto, indicando visualmente que a senha está sendo mostrada.
 
     } else {
@@ -25,27 +25,65 @@ function mostrarSenha(idInput, icone) {
         input.type = "password";
         // altera o atributo 'type' de volta para "password", escondendo novamente o texto.
 
-        icone.src = "../imgs/olho-fechado-senha.png";
+        icone.src = "../imgs/olho-aberto-senha.png";
         // muda o 'src' do ícone para a imagem do olho fechado, indicando que a senha está oculta.
     }
 }
 
 //=== Função Entrar/Login ===
+    var chkemail = false;
+    var chksenha = false;
+
+function onkey_email() {
+    email = ipt_email.value.trim();
+    let erro = "";
+
+    if (email == '') {
+        erro = `Preencha o campo Email`;
+    }  else if (email.indexOf("@") == -1) {
+        erro = `Insira um email válido que contenha @`;
+    }
+
+    if (erro != "") {
+        div_msg_email.innerHTML = `${erro}`;
+        chkemail = false;
+    } else {
+        div_msg_email.innerHTML = ``;
+        chkemail = true;
+    }
+}
+
+function onkey_senha() {
+    senha = ipt_senha.value.trim();
+    let erro = "";
+
+    if (senha == '') {
+        erro = `Preencha o campo Senha`;
+    } else if (senha.indexOf("0") == -1 && senha.indexOf("1") == -1 && senha.indexOf("2") == -1 &&
+            senha.indexOf("3") == -1 && senha.indexOf("4") == -1 && senha.indexOf("5") == -1 &&
+            senha.indexOf("6") == -1 && senha.indexOf("7") == -1 && senha.indexOf("8") == -1 &&
+            senha.indexOf("9") == -1){
+                erro = 'A senha deve conter pelo menos um número!';
+            }
+
+    if (erro != "") {
+        div_msg_senha.innerHTML = `${erro}`;
+        chksenha = false;
+    } else {
+        div_msg_senha.innerHTML = ``;
+        chksenha = true;
+    }
+}
+
 function login() {
+    // if simulando um cadastro
     var email = ipt_email.value;
     var senha = ipt_senha.value;
-
-    //Verificação de inputs em branco
-    if (email == "") {
-        div_msg_email.innerHTML = '* O campo Email não pode estar em branco!';
-    } else if (senha == "") {
-        div_msg_senha.innerHTML = '* O campo Senha não pode estar em branco!';
-    }
-    // if simulando um cadastro
-    else if (email == 'fernanda@sptech' && senha == '123456789') {
+    if (email == 'fernanda@sptech' && senha == '123456789') {
         alert('Login realizado com sucesso! Bem vindo a V3T.');
         window.location.href = "dashboard.html";
     } else {
-        div_msg_login.innerHTML = '* Email ou senhas invalidos, tente novamente!';
+        div_msg_login.innerHTML = ' Email ou senhas invalidos, tente novamente!';
     }
 }
+
